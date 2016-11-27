72e86da7be3f1c1855cd13c8f70e9429f5698b5c
Swornim Shrestha <srestaswrnm@www.schoophile.dev>
2016-11-27 15:04:24 +0545

    Minor Changes

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # nepali-names [![Build Status](https://travis-ci.org/bravegurkha/nepali-names.svg?branch=master)](https://travis-ci.org/bravegurkha/nepali-names)
 
-Get Unique Nepali names.
+Get popular nepali names.
+
+# How To Install
+
+```sh
+
+  $ npm install -g nepai-names
+
+```
+
+#How to use
+
+To get random male and female names. randomMale() andn randomFemale() function can be used.
+
+```js
+
+  let randNames = require('nepali-names');
+  randNames.randomMale() // To get random male names
+
+```
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
 let UniqueRandomArray = require('unique-random-array');
-let NepaliNames = require('./nepali-names.json');
+let femaleNames = require('./female-names.json');
+let maleNames = require('./male-names.json')
+let nameList = [femaleNames,maleNames];
 
-exports.all = NepaliNames;
-exports.random = UniqueRandomArray(NepaliNames);
+exports.all = nameList;
+exports.randomMale = UniqueRandomArray(maleNames);
+exports.randomFemale = UniqueRandomArray(femaleNames);
+exports.allMale = maleNames;
+exports.allFemale = femaleNames;
